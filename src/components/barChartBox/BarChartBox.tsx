04fe7b2b28a10b1f2bca85f782c5c6f8f140c462
simo <wambuisimon712@gmail.com>
2023-10-29 23:46:56 +0300

    good night

--- src/components/barChartBox/BarChartBox.tsx
+++ src/components/barChartBox/BarChartBox.tsx
@@ -13,11 +13,12 @@ const BarChartBox = (props: Props) => {
     <div className="barChartBox">
       <h1>{props.title}</h1>
       <div className="chart">
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart width={150} height={40} data={props.chartData}>
+        <ResponsiveContainer width="100%" height={150}>
+          <BarChart data={props.chartData}>
             <Tooltip
               contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
               labelStyle={{ display: "none" }}
+              cursor={{ fill: "none" }}
             />
             <Bar dataKey={props.dataKey} fill={props.color} />
           </BarChart>
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,8 +1,65 @@
+import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
 import "./users.scss";
-import React from "react";
+import { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 40 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 90,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 100,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 100,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    type: "string",
+    width: 150,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    type: "string",
+    width: 150,
+  },
+  {
+    field: "createdAt",
+    headerName: "created At",
+    width: 150,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    type: "boolean",
+    width: 80,
+  },
+];
 
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
 };
 
 export default Users;
